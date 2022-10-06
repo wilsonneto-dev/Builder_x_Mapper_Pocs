f3df13dc4102e1e7e3d32877e75b839e5c1aefbc
Wilson Neto <wilson.neto@dreamdev.io>
2022-10-06 13:37:04 +0100

    tetsing

---
+++ TestingBuilder/Models.cs
@@ -0,0 +1,32 @@
+﻿namespace TestingBuilder;
+
+// Carrier DTO
+public record RegisterShipmentInputDto
+{
+    public string ConsignorAddress { get; set; }
+    public string ConsignorName { get; set; }
+    public string ConsignorReference { get; set; }
+    public string ConsigneeAddress { get; set; }
+    public string ConsigneeName { get; set; }
+    public string ConsigneeReference { get; set; }
+    public decimal InsuranceValue { get; set; }
+    public List<string> LineItems { get; set; }
+}
+
+// Ordering DTO
+public record UpsertShipmentDto(
+    Guid Id,
+    string Status,
+    DateTime Date,
+    Person Retailer,
+    Person Shopper,
+    Delivery? Delivery,
+    IEnumerable<Item> Items);
+
+public record Person(int Id, string Name, string Address);
+
+public record Item(int ProductId, int Quantity, decimal Value);
+
+public record Delivery(string Status);
+
+public record GetOrderInput(Guid OrderId);
---
+++ TestingBuilder/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestingBuilder;
+
+var upsaertDto = new UpsertShipmentDto(
+    Guid.NewGuid(), 
+    "Active", 
+    DateTime.Now, 
+    new(1, "Retailer", "Address Retailer"), 
+    new(2, "Customer", "Customer Address"), 
+    null, 
+    new List<Item> { 
+        new(1, 1, 200)});
+
+// approach #1 -> passing the builder to maps
+var builder = new RegisterShipmentInputDtoBuilder();
+
+ConsignorMap.Map(upsaertDto.Retailer, builder);
+ConsigneeMap.Map(upsaertDto.Shopper, builder);
+LineItemsMap.Map(upsaertDto.Items, builder);
+
+var carrierDto = builder.GetResult();
+
+// ------------------
+
